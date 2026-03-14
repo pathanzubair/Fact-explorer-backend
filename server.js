5f4e3db993dfe48bc3c5aa7e79021e0e4ae25c0e
Zubair Pathan <pathanzubairaziz@gmail.com>
2026-03-14 13:17:36 +0530

    updated db

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Force Google DNS to resolve SRV records
+
+require('dotenv').config();
+// ... rest of your code
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
